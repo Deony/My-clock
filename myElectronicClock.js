0444236110c6cdc6660036aff1e104f6f1e15497
Deony <nostopmicky@gmail.com>
2018-03-28 21:43:13 +0300

    add the alarm

--- myElectronicClock.js
+++ myElectronicClock.js
@@ -1,6 +1,30 @@
 var hourHand = document.querySelector('.hour'),
     minuteHand = document.querySelector('.minute'),
-    secondHand = document.querySelector('.second');
+    secondHand = document.querySelector('.second'),
+    alarmSign = document.querySelector('.alarmSign'),
+    setAlarmTime = document.querySelector('.setAlarmTime'),
+    timeAlarmHour = document.querySelector('.timeAlarmHour'),
+    timeAlarmMinute = document.querySelector('.timeAlarmMinute'),
+    myAudio = new Audio();
+    myAudio.src = 'Ofenbach_-_Katchi.mp3',
+    timeEndAlarm = 20;    
+
+alarmSign.addEventListener('click', function(){setAlarm()}, false);
+alarmSign.addEventListener('dblclick', function(){deactivateAlarm()}, false);
+
+function setAlarm() {
+    alarmSign.style.opacity = 1;
+    setAlarmTime.style.display = 'block';
+    timeAlarmHour.style.display = 'block';
+    timeAlarmMinute.style.display = 'block';
+}
+
+function deactivateAlarm() {
+    alarmSign.style.opacity = 0.4;
+    setAlarmTime.style.display = 'none';
+    timeAlarmHour.style.display = 'none';
+    timeAlarmMinute.style.display = 'none';
+}
 
 function setDate() {
     var now = new Date(),
@@ -8,7 +32,7 @@ function setDate() {
         minutes = now.getMinutes(),
         hours = now.getHours();
 
-        hourHand.innerText = hours;
+        
         if(seconds < 10) {
         	seconds = '0' + seconds;
         }
@@ -18,9 +42,22 @@ function setDate() {
         if(hours < 10) {
         	hours = '0' + hours;
         }
+
+        hourHand.innerText = hours;
         minuteHand.innerText = minutes;
         secondHand.innerText = seconds;
 
+        if(timeAlarmHour.value==hours && timeAlarmMinute.value==minutes){
+            audioLoad();
+            var setTime = setTimeout(function() {              
+                    myAudio.pause();
+                }, 5000);
+        }
+
+}
+
+function audioLoad() {
+    myAudio.play();
 }
 
 setInterval(setDate);
